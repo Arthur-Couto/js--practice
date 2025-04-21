ded69e20e9def4cbe4f5240dee486d23ab508c5e
Arthur Do Couto <99839981+Arthur-Couto@users.noreply.github.com>
2025-04-21 01:11:46 -0300

    Create app2.js

---
+++ ascending order/app2.js
@@ -0,0 +1,6 @@
+//actually a function this time, ordering the parameters
+
+function ordenarNumeros(a, b, c) {
+    const numerosOrdenados = [a, b, c].sort((x, y) => x - y);
+    console.log(`Números ordenados: ${numerosOrdenados.join(', ')}`);
+}
