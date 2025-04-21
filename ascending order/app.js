bb82ff7d6e567688138989d71a9886b6329ec4ec
Arthur Do Couto <99839981+Arthur-Couto@users.noreply.github.com>
2025-04-21 01:08:58 -0300

    Update app.js

--- ascending order/app.js
+++ ascending order/app.js
@@ -1,21 +1,27 @@
-const sortNumber() => Math.floor(( Math.random() * 10) + 1;
 let orderedList = [];
 let list = [];
-let quantity = parseInt(window.prompt('how many numbers in this list? (min: 2; max: 10)'));
+let quantity = parseInt(window.prompt('how many numbers in this list? (min: 2; max: 10)'));  //could use Number() to turn string into number
 let number;
 
+//checking if prompt is a valid input
 if (isNaN(quantity) || quantity < 1 || quantity > 10) {
-    console.log('Please enter a valid number between 1 and 10.');
+    console.log('Please enter a valid number between 2 and 10.');
     return;
+} else {
+    //aplication of loops
+    for (let i = 0; i < quantity; i++) {
+        number = sortNumber();
+        while (list.includes(number)) {
+            number = sortNumber();
+        }
+        list.push(number)
+    }
+
+    //[...list] makes a copy of the array list before ordering, keeping the original array unaltered 
+    orderedList = [...list].sort((a, b) => a - b);
+    console.log('Original list:', list);
+    console.log('Ordered list:', orderedList);
 }
 
-for (let i = 0; i < quantity; i++) {
-  number = sortNumber();
-  list.push(number);
-}
-
-function orderList(quantity) {
-
-}
-
-
+//using arrow function
+const sortNumber = () => Math.floor(Math.random() * 10 + 1);
